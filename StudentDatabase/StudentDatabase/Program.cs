4d12824116acb83caa9eefb8c00686ac9cc1202d
Jered O'Grady <jered9715@gmail.com>
2024-02-08 21:16:14 -0500

    pseudocode

--- StudentDatabase/StudentDatabase/Program.cs
+++ StudentDatabase/StudentDatabase/Program.cs
@@ -1,1 +1,47 @@
-﻿
+﻿//Welcome user to the student database
+
+//Create student name array; all length of array based off this
+
+//Create hometown array; make sure index order matches previous
+
+//Create Favorite Food array; make sure index order matches previous
+
+//while loop starts
+
+//Tell user they can search students
+//Tell user they will be given the option to learn more about the student selected
+
+//give user option to see a list of the students
+
+//ask user if they would like to look up by number or student name
+
+//if student name request input of name
+
+//store index number to allow for reference to pull other info
+
+//display you chose x student
+
+//if user number request usernumber
+
+//ask user for student number input; this will base the index going forward
+
+//store student number for referrence later as var
+
+//print student name
+
+//ask user if they want to know hometown or favorite food
+
+//diplay relevant request using student name as sentence
+
+//ask if the user would like to look up another student 
+
+//if yes loop; clear console
+
+//if no end program; say goodbye
+
+
+
+//method for case sensitivity and portion of word?
+//metod for validation of user #
+//method for validation of fun fact category
+//Method for student Number(index number - 1)
